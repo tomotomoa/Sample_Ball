de9579d0d27628792d72380f194a80c757531812
imaze <tttttttt652@gmail.com>
2020-05-05 09:33:29 +0900

    TheBallプロジェクト

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Startボタンを押した
+    public void PushStartButton() {
+        //ステージ選択シーンへ
+        SceneManager.LoadScene("StageSelectScence");
+    }
+}
